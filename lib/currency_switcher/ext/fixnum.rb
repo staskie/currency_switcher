ff17c2b9260c4253362ac989bc2ba779bc69bb82
staskie <stadominik@gmail.com>
2011-01-07 00:21:31 +0100

    adding Rake for building a gem and updating Fixnum documentation

--- lib/currency_switcher/ext/fixnum.rb
+++ lib/currency_switcher/ext/fixnum.rb
@@ -1,5 +1,19 @@
 class Numeric
+  # Overriding method_missing to add CurrencySwitcher methods
+  #
+  # sym - standard method_missing argument
+  # *args - standard method_missing argument
+  # &block - standard method_missing argument
+  #
+  # Examples
+  #
+  #   3.usd_to_eur
+  #   1200.gbp_to_pln
+  #
+  # Returns Float value of currency exchange result
+  # Raises NoMethodError if couldn't find a method
   def method_missing(sym, *args, &block)
+    # Check if the missing method applies to CurrencySwitcher module
     if CurrencySwitcher.valid_exchange_method?(sym)
       return CurrencySwitcher.calculate_value(self)
     else
